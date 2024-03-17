3b11989ad8988a96b87130ce1e4f456f1866913f
yash <khatiyash010703@gmail.com>
2024-03-17 20:32:44 +0530

    recommit

---
+++ Level-1/Arrays/Product of Array Except Self.py
@@ -0,0 +1,22 @@
+
+nums = [-1,1]
+n = len(nums)
+pref_prod = [nums[0]]
+post_prod = [nums[n - 1]]
+for i in range(1, n):
+    pref_prod.append(pref_prod[i - 1] * nums[i])
+    post_prod.append(post_prod[i - 1] * nums[n - i - 1])
+
+post_prod.reverse()
+ans = []
+for i in range(n):
+    if i == 0:
+        ans.append(post_prod[i + 1])
+        continue
+    if i == n - 1:
+        ans.append(pref_prod[n - 2])
+        continue
+
+    ans.append(pref_prod[i - 1] * post_prod[i + 1])
+
+print(ans)
--- Level-2/Graphs/Min Swap to Sort Array.py
+++ Level-2/Graphs/Min Swap to Sort Array.py
@@ -1,29 +1,28 @@
 nums = [6, 4, 2, 14, 8, 10, 12, 16]
 
-print("Array : ",nums)
+print("Array : ", nums)
 
 elementPos = []
 for i in range(len(nums)):
-    elementPos.append((i,nums[i]))
+    elementPos.append((i, nums[i]))
 print("Before Sorting element position array : ", elementPos)
-
 
 elementPos.sort(key=lambda x: x[1])
 print("After Sorting element position array :  ", elementPos)
 
 vis = [False] * len(nums)
 ans = 0
-for i in range( len(nums)):
+for i in range(len(nums)):
     if vis[i] == True or elementPos[i][0] == i:
         continue
     else:
         cycle_len = 0
         j = i
-        while vis[j] == False:
+        while not vis[j]:
             vis[j] = True
             cycle_len += 1
             j = elementPos[j][0]
 
-        ans += (cycle_len-1)
+        ans += (cycle_len - 1)
 
 print(ans)
